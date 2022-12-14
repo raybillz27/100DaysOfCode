a88a6dfb2e621dfa109260a121990655b4f7a976
Chukwuelotam Ezechukwu <elotam@macbook-pro.local>
2022-12-14 23:55:16 +0100

    Python code for day 1

---
+++ day_1/day_1.py
@@ -0,0 +1,6 @@
+#band name generator
+print("welcome to the band name generator")
+city = input("which city do you grow up in?\n ")
+pet = input("what is the name of your pet?\n")
+input("the name of my band is.....")
+combine = print(city + pet)
