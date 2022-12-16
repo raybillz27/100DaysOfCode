201da8abb2c063f4fe6107682e450cef4a4c9044
Chukwuelotam Ezechukwu <112827438+raybillz27@users.noreply.github.com>
2022-12-16 10:40:53 +0100

    python codes day_3

---
+++ day_3/day_3_1.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+even_odd = number % 2
+if even_odd == 0:
+    print("even")
+else:
+    print("odd")
+
+
+
