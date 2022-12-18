c3a434eb38416007d155ab2744ddfcc01d5089de
Chukwuelotam Ezechukwu <112827438+raybillz27@users.noreply.github.com>
2022-12-18 01:40:24 +0100

    python codes day_1

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -2,6 +2,6 @@
 print("welcome to the band name generator")
 city = input("which city do you grow up in?\n ")
 pet = input("what is the name of your pet?\n")
-input("the name of my band is.....")
+input("your band  name is.....")
 brand_name = f"{city} {pet}"
 print(brand_name)
