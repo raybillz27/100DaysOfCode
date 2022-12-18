d69565c2e75f41bd7b2565ff54bf7462f9491a2e
Chukwuelotam Ezechukwu <112827438+raybillz27@users.noreply.github.com>
2022-12-18 01:24:38 +0100

    python codes day_4

---
+++ day_4/day_4_2.py
@@ -0,0 +1,20 @@
+
+# Import the random module here
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma.\n ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random
+
+len(names)
+
+rand_int = random.randint(1,4)
+payee = names[rand_int]
+print(f"{payee} will buy the meal for today")
+
+
+
+
