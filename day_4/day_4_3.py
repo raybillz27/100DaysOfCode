bd53a2aad92b29e46055f3750cc7c76710079078
Chukwuelotam Ezechukwu <112827438+raybillz27@users.noreply.github.com>
2022-12-20 07:20:41 +0100

    python codes day_4

--- day_4/day_4_3.py
+++ day_4/day_4_3.py
@@ -4,7 +4,7 @@ row3 = ["⬜️️","⬜️️","⬜️️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 
-position = input("Where do you want to put the treasure? ")
+position = input("Where do you want to put the treasure?\n")
 
 horizontal = int(position[0])
 vertical = int(position[1])
