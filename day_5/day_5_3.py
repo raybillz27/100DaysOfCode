b467a69c926dcc1635137637c70da944fbbfd7e8
Chukwuelotam Ezechukwu <112827438+raybillz27@users.noreply.github.com>
2022-12-20 13:43:16 +0100

    python codes day_5

---
+++ day_5/day_5_3.py
@@ -0,0 +1,16 @@
+#Write your code below this row 👇
+total = 0 
+for number in  range(2,101,2):
+   total += number
+  
+print(total)
+
+
+#or you can also 
+total2 = 0
+for number in range(1,101):
+    if number % 2 == 0:
+      total2 += number  
+print(total2)
+
+36985028
