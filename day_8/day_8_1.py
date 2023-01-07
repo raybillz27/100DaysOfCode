1f2d0940ad92b9610f4a6ed44f5797874ae744e5
Chukwuelotam Ezechukwu <112827438+raybillz27@users.noreply.github.com>
2023-01-07 20:34:13 +0100

    python codes day_8

---
+++ day_8/day_8_1.py
@@ -0,0 +1,60 @@
+#PYTHON DICTIONARY
+
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again."
+}
+
+#retrieving items from a dictionary
+print(programming_dictionary["Bug"])
+
+# adding new items to a dictionary
+programming_dictionary["loop"] = "the action of doing someting repeatedly by a computer"
+
+print(programming_dictionary)
+
+#create an empty dictionary
+programming_dictionary = {}
+
+#modify an item in a computer
+programming_dictionary["Bug"] = "a moth in a computer"
+print(programming_dictionary)
+
+#loop into a dictionary
+
+for key in programming_dictionary:
+ print(key)
+ print(programming_dictionary[key])
+
+
+
+#wipe an existing dictionary
+programming_dictionary = {}
+print(programming_dictionary)
+
+#dictionary nesting
+{
+  "country":["poland","italy","nigeria"],
+  "fruits":["banana","orange","strawberry"]
+  }
+
+  #nesting a dictionary in a dictionary
+
+travel_log = {
+  "country":{"country_visited":["poland","italy","nigeria"],"total_visits": 12},
+  "fruits":{"fruits_eaten":["banana","orange","strawberry"],"total":16},
+}
+
+#nesting dictionary inside a list
+travel_log = [
+  {
+    "travel log":"country",
+    "country_visited":["poland","italy","nigeria"],
+    "total_visits": 12
+    },
+  {
+    "fruits":"natural",
+    "fruits_eaten":["banana","orange","strawberry"],
+    "total":16
+    },
+]
---
+++ day_8/day_8_2.py
@@ -0,0 +1,27 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+
+student_grades = {}
+
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+for students_grades in student_scores:
+
+   student_grades["Harry"] = "exceeds expectations" 
+student_grades["Ron"] = "acceptable"
+student_grades["Hermione"] = "outstanding"
+student_grades["Draco"] = "acceptable"
+student_grades["Neville"] = "fail"
+
+# 🚨 Don't change the code below 👇
+
+print(student_grades)
