ce5a9146fd1634ba2e672f331fb83aa5702461db
Chukwuelotam Ezechukwu <112827438+raybillz27@users.noreply.github.com>
2023-02-03 11:43:54 +0100

    python codes

---
+++ day_14.py/day_14_2.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+tinny = Turtle()
+print(tinny)
+tinny.shape("turtle")
+tinny.color("green", "red")
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
